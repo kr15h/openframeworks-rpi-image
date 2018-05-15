688c6811223e57686ff096996b86dcb0b31626da
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-05-15 08:15:57 +0200

    Add compile and deploy stage

---
+++ image-compile-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello. This is compile script."
+
+echo "Compiling openFrameworks."
+make -j$(nproc) Release -C /home/pi/openFrameworks/libs/openFrameworksCompiled/project
+
+echo "Congratulations! openFrameworks $OF_VERSION has been compiled."
